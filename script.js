f806a453165cd29c4d01afce82cdb3e43599548d
wolfvain305 <dwolf0305@gmail.com>
2024-06-09 20:39:06 -0400

    exercise 3

--- script.js
+++ script.js
@@ -67,3 +67,74 @@ console.log('Exercise 1 Correct Result: ',
         { first: 'Johannes', last: 'Kepler', year: 1571, passed: 1630 }
     ]
 )
+
+// Array.prototype.map()
+
+// 2. Map the array of inventors into a new array. This new array should only contain objects with the inventors' first and last names.
+
+// - For each inventor, return an object in the format { first: "First Name", last: "Last Name" }.
+// - The new array should be a collection of these objects, each representing an inventor with only their first and last names.
+
+// Hint:  Return a new object literal from the callback that looks something like { first: "First Name", last: "Last Name" }
+
+
+let inventorNames = []
+
+// Enter your solution code here:
+
+inventorNames = inventors.map(inventor => {
+    return {
+        first: inventor.first,
+        last: inventor.last
+    }
+}) 
+
+// Check your return value:
+console.log('Exercise 2 My Result: ', inventorNames)
+console.log('Exercise 2 Correct Result: ',
+    [
+        { first: 'Albert', last: 'Einstein' },
+        { first: 'Isaac', last: 'Newton' },
+        { first: 'Galileo', last: 'Galilei' },
+        { first: 'Marie', last: 'Curie' },
+        { first: 'Johannes', last: 'Kepler' },
+        { first: 'Nicolaus', last: 'Copernicus' },
+        { first: 'Max', last: 'Planck' },
+        { first: 'Katherine', last: 'Blodgett' },
+        { first: 'Ada', last: 'Lovelace' },
+        { first: 'Sarah E.', last: 'Goode' },
+        { first: 'Lise', last: 'Meitner' },
+        { first: 'Hanna', last: 'Hammarström' }
+    ]
+)
+
+// Array.prototype.sort()
+
+// 3. Sort the inventors by birth date in ascending order (from those born furthest in the past to those born most recently).
+
+let sortedByBirthYear = []
+
+// Enter your solution code here:
+
+sortedByBirthYear = inventors.sort((a, b) => {
+    return parseInt(a.year) - parseInt(b.year)
+})
+
+// Check your return value:
+console.log('Exercise 3 My Result: ', sortedByBirthYear)
+console.log('Exercise 3 Correct Result: ',
+    [
+        { first: 'Nicolaus', last: 'Copernicus', year: 1473, passed: 1543 },
+        { first: 'Galileo', last: 'Galilei', year: 1564, passed: 1642 },
+        { first: 'Johannes', last: 'Kepler', year: 1571, passed: 1630 },
+        { first: 'Isaac', last: 'Newton', year: 1643, passed: 1727 },
+        { first: 'Ada', last: 'Lovelace', year: 1815, passed: 1852 },
+        { first: 'Hanna', last: 'Hammarström', year: 1829, passed: 1909 },
+        { first: 'Sarah E.', last: 'Goode', year: 1855, passed: 1905 },
+        { first: 'Max', last: 'Planck', year: 1858, passed: 1947 },
+        { first: 'Marie', last: 'Curie', year: 1867, passed: 1934 },
+        { first: 'Lise', last: 'Meitner', year: 1878, passed: 1968 },
+        { first: 'Albert', last: 'Einstein', year: 1879, passed: 1955 },
+        { first: 'Katherine', last: 'Blodgett', year: 1898, passed: 1979 }
+    ]
+)
